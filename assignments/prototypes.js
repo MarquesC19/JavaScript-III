5877d5872dbc73fa1e68bb076b5c4168a6c0d0c6
MarquesC19 <Carter234>
2019-09-18 17:41:07 -0500

    still not complete yet

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -75,80 +75,21 @@ function Hero(heroAttributes){
     return `A direct hit! ${character.takeDamage(character)} and only has ${character.healthPoints}hp remaining.
   };
 
-  
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
 
-// Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-// 
-const mage= new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+  // ________________Villain________________
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+function Villain(villainAttributes){
+  Humanoid.call(this, villainAttributes);
+  }
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+Villain.prototype = Object.create(Humanoid.prototype);
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+villain.prototype.crash=function crash(character){
+  character.healthPoints= character.healthPoints -7;
+  if(character.healthPoints <=0) {
+    return charaacter.destroy;
+  }else
+  return `A direct hit! ${character.takeDamage(character)} and only has ${character.healthPoints} hp remaining.`
+};
